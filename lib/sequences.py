e0c5140df4eea4d07f6eeee3effeacd61c4473a9
mimodz3 <sid-rah@live.fr>
2023-11-17 21:46:45 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    if length == 0:
+        print('[]')
+    elif length == 1:
+        print('[0]')
+    else:
+        fib_sequence = [0, 1]
+        for i in range(2, length):
+            fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i - 2])
+        print(fib_sequence)
+
     pass
